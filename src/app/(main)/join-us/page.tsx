8599909ed53fc6975bdc11e91d0862d4ba3984f0
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-09-21 20:58:14 +0200

    feat(app): add the basic routing folder structure for the main and auth groups

---
+++ src/app/(main)/join-us/page.tsx
@@ -0,0 +1,3 @@
+export default function JoinUsPage() {
+  return <main>Join Us Page</main>
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
       <h1>Welcome to Rubble!</h1>

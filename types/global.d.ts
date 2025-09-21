041dab546bbb1b0640303409237f1e425d80a228
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-09-21 20:43:17 +0200

    chore: add global declare types, add it the tsconfig.json

---
+++ types/global.d.ts
@@ -0,0 +1,24 @@
+// https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-d-ts.html
+
+// Type definitions for Rubble
+// Definitions by: Vladyslav <https://github.com/hexlabelposition>
+
+declare global {
+  /**
+   * Represents a type that can be either the specified type or null.
+   */
+  type Nullable<T> = T | null
+
+  /**
+   * Represents a type that can be either the specified type or undefined.
+   */
+  type Optional<T> = T | undefined
+
+  /**
+   * Represents a type of a generic dictionary.
+   */
+  type Dictionary<T = unknown> = Record<string, T>
+}
+
+// make a file a module
+export {}

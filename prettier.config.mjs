fe1c910e77b743531314cfa1db0de5b7080856dd
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-09-21 20:13:13 +0200

    chore: add prettier, prettier config and plugin for eslint dependency; add prettier config and ignore files; add scripts format and check format to package.json

---
+++ prettier.config.mjs
@@ -0,0 +1,19 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config

b8028d88de4867e705d255f49b18518f0e37e272
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-09-21 20:44:31 +0200

    feat(app): connect Manrope font and global css file to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,16 @@
+import { Manrope } from 'next/font/google'
 import type { ReactNode } from 'react'
+import './global.css'
+
+const manrope = Manrope({
+  variable: '--font-manrope',
+  subsets: ['latin'],
+  style: 'normal',
+  display: 'swap',
+  preload: true,
+  fallback: ['Inter', 'Rubik', 'Arial', 'sans-serif'],
+  adjustFontFallback: true,
+})
 
 interface RootLayoutProps {
   children: ReactNode
@@ -6,7 +18,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
-    <html lang='en'>
+    <html lang='en' data-scroll-behavior='smooth' className={manrope.className}>
       <body>{children}</body>
     </html>
   )

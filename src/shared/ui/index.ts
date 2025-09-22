b55be373f88623c4de5b84078dec84a1c8ec61e2
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-09-23 01:56:13 +0200

    feat(shared): add button component to ui kit

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,6 @@
+// module: kit
+export { Button } from './kit/Button'
+
 // module: svg
 export { MenuIcon } from './svg/MenuIcon'
 export { CloseIcon } from './svg/CloseIcon'
---
+++ src/shared/ui/kit/Button.tsx
@@ -0,0 +1,50 @@
+import { tv } from 'tailwind-variants'
+
+import type { ElementType } from 'react'
+import type { VariantProps } from 'tailwind-variants'
+import type { PolymorphicProps } from '@shared/type'
+
+const buttonVariants = tv({
+  base: [
+    'inline-flex shrink-0 items-center justify-center px-8',
+    'whitespace-nowrap',
+    'border xl:border-2',
+    '[&_svg]:pointer-events-none [&_svg]:shrink-0',
+  ],
+  variants: {
+    variant: {
+      primary: 'text-primary-0 bg-primary-500 border-primary-300',
+      secondary: 'text-primary-500 bg-primary-0 border-secondary-200',
+      blur: 'text-primary-0 bg-primary-500/24 border-transparent backdrop-blur',
+    },
+    size: {
+      'extra-small': 'min-h-16 rounded-sm text-sm font-medium',
+      small: 'min-h-18 rounded-sm text-sm font-medium',
+      medium: 'min-h-20 rounded text-base font-semibold',
+      large: 'min-h-24 rounded text-base font-semibold',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'medium',
+  },
+})
+
+type ButtonProps<T extends ElementType = 'button'> = PolymorphicProps<T> &
+  VariantProps<typeof buttonVariants>
+
+export const Button = <T extends ElementType = 'button'>({
+  as,
+  variant = 'primary',
+  size = 'medium',
+  className,
+  ...props
+}: ButtonProps<T>) => {
+  const Component = as || 'button'
+  return (
+    <Component
+      className={buttonVariants({ variant, size, className })}
+      {...props}
+    />
+  )
+}

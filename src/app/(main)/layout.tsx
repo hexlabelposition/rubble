200af29a5e964e18108c3a5cb8dbe5e6b03c1ae9
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-09-23 03:24:56 +0200

    feat(app): connect main header and footer to main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,18 @@
+import { MainHeader } from '@widgets/header/main'
+import { MainFooter } from '@widgets/footer/main'
+
 import type { ReactNode } from 'react'
 
 interface MainLayoutProps {
   children: ReactNode
 }
 
-export default function MainLayout({ children }: MainLayoutProps) {
+export default function MainLayout({ children }: Readonly<MainLayoutProps>) {
   return (
-    <div className='container'>
-      <header>Header</header>
-      {children}
-      <footer>Footer</footer>
+    <div className='container flex min-h-screen flex-col'>
+      <MainHeader />
+      <div className='flex-1'>{children}</div>
+      <MainFooter />
     </div>
   )
 }

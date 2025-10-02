fb012202114e9cdb2bd16db8fe8413349d06604a
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-10-02 22:46:47 +0200

    feat(app): add basic metadata for root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,24 @@ import { Inter } from 'next/font/google'
 import { Figtree } from 'next/font/google'
 
 import type { ReactNode } from 'react'
+import type { Metadata } from 'next'
 
 import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Rubble — A Modern Real Estate Platform',
+  description:
+    'Rubble is an innovative platform for buying, selling, and renting real estate. Find properties easily, explore up-to-date listings, and use simple tools for both owners and buyers.',
+  keywords: [
+    'real estate',
+    'buy property',
+    'sell property',
+    'rent property',
+    'property listings',
+    'modern platform',
+  ],
+  robots: 'index, follow',
+}
 
 const manrope = Manrope({
   variable: '--font-manrope',

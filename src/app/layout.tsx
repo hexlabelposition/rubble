7841dcd70148ab9b52c15b4c875fe7247a00c1d6
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-10-02 22:35:14 +0200

    feat(app): connect manrope, inter, figtree fonts to root layotu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,31 @@
+import { Manrope } from 'next/font/google'
+import { Inter } from 'next/font/google'
+import { Figtree } from 'next/font/google'
+
 import type { ReactNode } from 'react'
 
 import './globals.css'
+
+const manrope = Manrope({
+  variable: '--font-manrope',
+  subsets: ['latin'],
+  style: 'normal',
+  display: 'swap',
+})
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  style: 'normal',
+  display: 'swap',
+})
+
+const figtree = Figtree({
+  variable: '--font-figtree',
+  subsets: ['latin'],
+  style: 'normal',
+  display: 'swap',
+})
 
 interface RootLayoutProps {
   children: ReactNode
@@ -8,7 +33,11 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      data-scroll-behavior='smooth'
+      className={`${manrope.variable} ${inter.variable} ${figtree.variable}`}
+    >
       <body>{children}</body>
     </html>
   )

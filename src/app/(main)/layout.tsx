f5bafeeefec12f30cc3894e1278a13f3d188ae25
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-10-03 20:34:11 +0200

    feat(app): add main layout with header and footer widgets

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import { MainHeader } from '@widgets/header/main'
+import { MainFooter } from '@widgets/footer/main'
+
+import type { ReactNode } from 'react'
+
+interface MainLayoutProps {
+  children: ReactNode
+}
+
+export default function MainLayout({ children }: Readonly<MainLayoutProps>) {
+  return (
+    <div className='container flex min-h-svh flex-col'>
+      <MainHeader />
+      <div className='flex-1'>{children}</div>
+      <MainFooter />
+    </div>
+  )
+}

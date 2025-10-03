220ea3ed18d9b444f1da0b5c6ef327b0454d514f
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-10-03 21:37:29 +0200

    feat(shared): add section component to ui kit

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,6 @@
 export { Button } from './kit/Button'
 export { Separator } from './kit/Separator'
 export { Card } from './kit/Card'
+export { Section } from './kit/Section'
 
 export { LogoIcon } from './icons/LogoIcon'

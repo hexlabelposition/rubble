34db5eb521cbaf9ecef194e8f195a0182806fab0
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-10-03 19:46:10 +0200

    chore: add tailwind function in prettier for tv and merge classes utilities, add support tailwind intellisense extension in tailwind-variants

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -14,6 +14,7 @@ const config = {
   bracketSameLine: false,
   arrowParens: 'always',
   plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['tv', 'mergeClasses'],
 }
 
 export default config

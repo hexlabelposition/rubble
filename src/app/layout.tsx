86b342ab80d4c59a92e7ec1b8949d2c2780ddda3
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-11-16 17:15:39 +0100

    chore: add basic routing to app router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,17 @@
+import { Manrope } from "next/font/google";
+
 import type { ReactNode } from "react";
+
+const manrope = Manrope({
+  variable: "--next-font-manrope",
+  subsets: ["latin"],
+  style: "normal",
+  // weight: [], Don't use. Variable font.
+  display: "swap",
+  preload: true,
+  adjustFontFallback: true,
+  fallback: ["sans-serif"],
+});
 
 import "./global.css";
 
@@ -8,7 +21,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth" className={manrope.variable}>
       <body>{children}</body>
     </html>
   );

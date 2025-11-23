0efc0c5854e0f130c1da811187f403c812e4bcf8
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-11-23 21:01:14 +0100

    feat(app): connect hero, about rubble, services we provide, popular property, how it work and contact us to main home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,19 @@
+import { Hero } from "@widgets/hero";
+import { AboutRubble } from "@widgets/about-rubble";
+import { ServicesWeProvide } from "@widgets/services-we-provide";
+import { PopularProperty } from "@widgets/popular-property";
+import { HowItWork } from "@widgets/how-it-work";
+import { ContactUs } from "@widgets/contact-us";
+
 export default function HomePage() {
-  return <main>Home Page</main>;
+  return (
+    <main>
+      <Hero />
+      <AboutRubble />
+      <ServicesWeProvide />
+      <PopularProperty />
+      <HowItWork />
+      <ContactUs />
+    </main>
+  );
 }

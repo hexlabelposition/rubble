616f9f685fed10f2beced60c3d85d767201dbc47
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-11-26 01:59:32 +0100

    feat(app): add fetching popular properties with mocks data

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,13 +5,17 @@ import { PopularProperty } from "@widgets/popular-property";
 import { HowItWork } from "@widgets/how-it-work";
 import { ContactUs } from "@widgets/contact-us";
 
-export default function HomePage() {
+import { getPopularProperty } from "@entities/property";
+
+export default async function HomePage() {
+  const properties = await getPopularProperty();
+
   return (
     <main>
       <Hero />
       <AboutRubble />
       <ServicesWeProvide />
-      <PopularProperty />
+      <PopularProperty properties={properties} />
       <HowItWork />
       <ContactUs />
     </main>

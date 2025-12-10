fa35db285b361808b5a505dcf74a91f99e39e45d
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-12-10 19:38:57 +0100

    feat(app): connect hero, about rubble and services we provide widgets to home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,13 @@
+import { Hero } from "@widgets/hero";
+import { AboutRubble } from "@widgets/about-rubble";
+import { ServicesWeProvide } from "@widgets/services-we-provide";
+
 export default function HomePage() {
-  return <main>Home page</main>;
+  return (
+    <main>
+      <Hero />
+      <AboutRubble />
+      <ServicesWeProvide />
+    </main>
+  );
 }

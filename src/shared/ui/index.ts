4bd9340696cb9a545c765ad238d8b719ff4064c8
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-12-10 18:28:16 +0100

    feat(shared): add move up right svg icon as react component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,3 +3,4 @@ export { Accordion } from "./kit/Accordion";
 export { Separator } from "./kit/Separator";
 export { LogoIcon } from "./icons/LogoIcon";
 export { MenuIcon } from "./icons/MenuIcon";
+export { MoveUpRightIcon } from "./icons/MoveUpRightIcon";

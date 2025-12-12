bbc941035a8729b04c902d6780efe566cefcd626
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-12-12 17:41:51 +0100

    feat(shared): add buildings, calendar days, dollar square and map pin check inside icons as react component

---
+++ src/shared/ui/icons/DollarSquareIcon.tsx
@@ -0,0 +1,27 @@
+import type { SVGProps } from "react";
+
+export const DollarSquareIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    width="1em"
+    height="1em"
+    fill="none"
+    {...props}
+  >
+    <path
+      d="M7.227 11.942c0 1.075.825 1.941 1.85 1.941h2.091c.892 0 1.617-.758 1.617-1.691 0-1.017-.442-1.376-1.1-1.609L8.327 9.416c-.659-.233-1.1-.591-1.1-1.608 0-.933.725-1.692 1.616-1.692h2.092c1.025 0 1.85.867 1.85 1.942M10 5v10"
+      stroke="currentColor"
+      strokeWidth={1.2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12.5 18.333h-5c-4.167 0-5.833-1.666-5.833-5.833v-5c0-4.167 1.666-5.833 5.833-5.833h5c4.167 0 5.833 1.666 5.833 5.833v5c0 4.167-1.666 5.833-5.833 5.833"
+      stroke="currentColor"
+      strokeWidth={1.2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -6,3 +6,7 @@ export { Section } from "./kit/Section";
 export { LogoIcon } from "./icons/LogoIcon";
 export { MenuIcon } from "./icons/MenuIcon";
 export { MoveUpRightIcon } from "./icons/MoveUpRightIcon";
+export { BuildingsIcon } from "./icons/BuildingsIcon";
+export { CalendarDaysIcon } from "./icons/CalendarDaysIcon";
+export { MapPinCheckInsideIcon } from "./icons/MapPinCheckInsideIcon";
+export { DollarSquareIcon } from "./icons/DollarSquareIcon";

0f86d0d4ab8ca8e7234f61969e3f56fbc684c948
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-12-12 17:52:28 +0100

    feat(app): connect popular property, how it work and contact us widgets to home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,9 @@
 import { Hero } from "@widgets/hero";
 import { AboutRubble } from "@widgets/about-rubble";
 import { ServicesWeProvide } from "@widgets/services-we-provide";
+import { PopularProperty } from "@widgets/popular-property";
+import { HowItWork } from "@widgets/how-it-work";
+import { ContactUs } from "@widgets/contact-us";
 
 export default function HomePage() {
   return (
@@ -8,6 +11,9 @@ export default function HomePage() {
       <Hero />
       <AboutRubble />
       <ServicesWeProvide />
+      <PopularProperty />
+      <HowItWork />
+      <ContactUs />
     </main>
   );
 }

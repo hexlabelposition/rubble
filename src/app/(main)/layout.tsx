88b23ae0d22cb7844547f690bb76be1e846d9abf
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-12-17 01:25:32 +0100

    feat(app): add main layout

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className="container flex min-h-svh flex-col">
+      <div className="grow">{children}</div>
+    </div>
+  );
+}

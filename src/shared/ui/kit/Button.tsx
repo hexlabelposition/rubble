41e4b963acbd0c6f730e34403b86d6de0404be15
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2025-12-19 00:59:36 +0100

    feat(shared): add a basic button component

---
+++ src/shared/ui/kit/Button.tsx
@@ -0,0 +1,52 @@
+import { tv } from "tailwind-variants";
+
+import type { ElementType } from "react";
+import type { ComponentPropsWithRef } from "react";
+import type { VariantProps } from "tailwind-variants";
+
+const buttonVariants = tv({
+  base: "inline-flex shrink-0 items-center justify-center whitespace-nowrap",
+  variants: {
+    variant: {
+      primary: "bg-primary-500 text-primary-0 border-primary-300",
+      secondary: "border-secondary-200 bg-primary-0 text-primary-500",
+    },
+    size: {
+      "extra-small": "min-h-16 rounded-sm border px-8 text-xs font-semibold",
+      small: "min-h-18 rounded-sm border px-8 text-sm font-semibold",
+      medium: "min-h-20 rounded-md border-2 px-8 text-base font-semibold",
+      large: "min-h-24 rounded-md border-2 px-8 text-base font-semibold",
+    },
+    withIcon: {
+      true: "gap-x-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+      false: null,
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "medium",
+    withIcon: false,
+  },
+});
+
+type ButtonProps<T extends ElementType> = {
+  as?: T;
+} & ComponentPropsWithRef<T> &
+  VariantProps<typeof buttonVariants>;
+
+export const Button = <T extends ElementType = "button">({
+  as,
+  variant,
+  size,
+  withIcon,
+  className,
+  ...props
+}: ButtonProps<T>) => {
+  const Component = as || "button";
+  return (
+    <Component
+      className={buttonVariants({ variant, size, withIcon, className })}
+      {...props}
+    />
+  );
+};
